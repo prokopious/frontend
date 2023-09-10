956d6a96b6ff41029f8c0c94b776ca7aeae3f23b
John Smith <cirrusnite@hotmail.com>
2023-09-09 22:51:27 -0400

    keyprops

--- components/MusicPlayer.js
+++ components/MusicPlayer.js
@@ -1,18 +1,14 @@
 "use client"
 import AudioPlayer from 'react-h5-audio-player';
-
 import './styles.css';
 import { useState, useEffect } from 'react'
 import styles from './MusicPlayer.module.css'
 
 export default function MusicPlayer({ title, data }) {
 
-
   const [track, setTrack] = useState("");
   const [trackName, setTrackName] = useState("");
   const [d, setD] = useState(data);
-
-
 
   const getTrack = (trackTitle) => {
     let obj = d.find(o => o.public_id === trackTitle);
@@ -20,16 +16,15 @@ export default function MusicPlayer({ title, data }) {
     return obj.url;
   }
 
-
   return (
     <div className={styles.player}>
       <div className={styles.track}>Podcast Episodes</div>
       <div className={styles.tracklist}>
 
-        {data.map((item) => {
+        {data.map((item, n) => {
 
           return (
-            <div className={trackName !== "" && trackName == item.public_id ? styles.bBox : styles.bBox2}>
+            <div key={n} className={trackName !== "" && trackName == item.public_id ? styles.bBox : styles.bBox2}>
               <button className={styles.buton} onClick={() => setTrack(getTrack(item.public_id))}>
                 {item.public_id}
               </button>
@@ -41,8 +36,6 @@ export default function MusicPlayer({ title, data }) {
         src={track}
         onPlay={e => console.log("onPlay")}
       />
-
-
     </div>
   )
 
